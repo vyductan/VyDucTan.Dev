b47fca4f5036c8df5fc02de98ca6bf83bd202a38
tk8226 <vdt5snet@gmail.com>
2021-08-05 21:34:43 +0700

    Update About

--- config/resumeData.ts
+++ config/resumeData.ts
@@ -2,11 +2,11 @@ export type ResumeData = typeof resumeData;
 export const resumeData = {
   main: {
     name: "Vy Duc Tan",
-    occupation: "Full Stack Web Developer",
+    occupation: "Full Stack Developer",
     description: "I eat, sleep and breathe React",
     image: "profilepic.jpg",
     bio:
-      "Hi there! I am Tan! A developer with experience in building websites for small and medium sized businesses. I can build the Web Application from scratch and responsive for Mobile.",
+      "Hi there! I am Tan! A Full Stack Developer with experience in building websites for small and medium sized businesses. With React, NodeJs, NextJs as well as tons of NoSQL knowledge, I have performed built dynamic front end interfaces and complicated back-end APIs or database integrations. I enjoy working with the various technologies on the internet. I will provide high quality results and hard work until you are satisfied.",
     contactMessage: "Get in touch with me if you need something from me!",
     email: "vdt5snet@gmail.com",
     phone: "+84 961034723",
@@ -19,14 +19,14 @@ export const resumeData = {
     website: "https://www.vyductan.me",
     resumeDownload: "http://vyductan.me",
     social: [
-      {
-        name: "youtube",
-        url: "https://www.youtube.com/channel/UC04Ncnn3QMFAVmqRXbTL7xA",
-        iconName: "Youtube",
-      },
+      // {
+      //   name: "youtube",
+      //   url: "https://www.youtube.com/channel/UC04Ncnn3QMFAVmqRXbTL7xA",
+      //   iconName: "Youtube",
+      // },
       {
         name: "facebook",
-        url: "https://www.facebook.com/vyductanme",
+        url: "https://www.facebook.com/vyductan",
         iconName: "Facebook",
       },
       {
@@ -47,7 +47,7 @@ export const resumeData = {
       {
         school: "QuyNhon University - VN",
         degree: "Bachelor's Degree of Infomation Technology",
-        graduated: "July 2021",
+        graduated: "July 2018",
         description:
           "Web Page Development, Operating Systems Architecture, Linux/Unix Programming, Data Engineering, Advanced Software Programming",
         // description: "Graduated with Average good Degree Classification",
@@ -56,7 +56,7 @@ export const resumeData = {
     certification: [
       {
         name: "Legacy Full Stack",
-        graduated: "July 2021",
+        graduated: "April 14, 2021",
         provider: "FreeCodeCamp",
       },
     ],
@@ -64,9 +64,9 @@ export const resumeData = {
       {
         company: "VyDucTan",
         title: "Founder, Developer and CEO",
-        years: "May 2021 - Present",
+        years: "March 2021 - Present",
         description:
-          "Build the application, website for customers. Share everything useful to the world!",
+          "Build the website for customers. Share everything useful to the world!",
       },
     ],
     skills: [
@@ -75,11 +75,19 @@ export const resumeData = {
         level: "100%",
       },
       {
+        name: "NodeJs",
+        level: "80%",
+      },
+      {
         name: "Git",
-        level: "70%",
+        level: "80%",
       },
       {
         name: "Html, Css, JavaScript",
+        level: "90%",
+      },
+      {
+        name: "Tailwind CSS",
         level: "90%",
       },
       {

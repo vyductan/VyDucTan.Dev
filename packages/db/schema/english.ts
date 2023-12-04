93765f303616cfd0cdfc7a39e11bce48a75ad7ab
VyDucTan <vdt5snet@gmail.com>
2023-12-04 11:42:27 +0700

    fix: Dependency {any} has multiple versions defined in the workspace

--- packages/db/schema/english.ts
+++ packages/db/schema/english.ts
@@ -1,18 +1,19 @@
-import { json, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { nanoid } from "nanoid";
 
 import { users } from "./_nextauth";
 
+// export const cefrLevelEnum = pgEnum("cefrLevel", ["C1", "known", "popular"]);
+type CEFRLevel = "C1" | "known" | "popular";
 type WordDefinition = {
   english: string;
   vietnamese: string;
+  cefrLevel: CEFRLevel;
 };
-export const cefrLevelEnum = pgEnum("cefrLevel", ["C1", "known", "popular"]);
 export const word = pgTable("word", {
   id: text("id").default(nanoid()).notNull().primaryKey(),
   name: text("name").notNull(),
 
-  level: cefrLevelEnum("level"),
   definitions: json("definitions").$type<WordDefinition[]>(),
 
   createdAt: timestamp("created_at").defaultNow().notNull(),

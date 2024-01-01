8e7240d8460b2acb473faee598f3f35048441537
VyDucTan <vdt5snet@gmail.com>
2024-01-01 17:46:32 +0700

    fix(projects/router.ts): import correct dependencies and remove unnecessary comments feat(router.ts): add support for retrieving project by slug and its tasks fix(schema.ts): add slug field to projects table and add status field to tasks table feat(types.ts): add types for project tasks and create insertTaskSchema

--- packages/api/src/projects/router.ts
+++ packages/api/src/projects/router.ts
@@ -1,32 +1,49 @@
 import { z } from "zod";
 
-// import { desc, eq, schema } from "../db.ts_.ts_.ts_";
-import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
-import { insertProjectSchema } from "./types";
+import { desc, eq, schema } from "../db";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { insertProjectSchema, insertTaskSchema } from "./types";
 
 export const projectsRouter = createTRPCRouter({
-  all: publicProcedure.query(({ ctx }) => {
-    // return ctx.db.query.projects.findMany({
-    //   orderBy: desc(schema.projects.createdAt),
-    //   limit: 10,
-    // });
+  all: protectedProcedure.query(({ ctx }) => {
+    return ctx.db.query.projects.findMany({
+      orderBy: desc(schema.projects.createdAt),
+      limit: 10,
+    });
   }),
 
-  byId: publicProcedure
+  byId: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(({ ctx, input }) => {
-      // return ctx.db.query.projects.findFirst({
-      //   where: eq(schema.projects.id, input.id),
-      // });
+      return ctx.db.query.projects.findFirst({
+        where: eq(schema.projects.id, input.id),
+      });
+    }),
+  bySlug: protectedProcedure
+    .input(z.object({ slug: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.query.projects.findFirst({
+        where: eq(schema.projects.slug, input.slug),
+        with: {
+          tasks: true,
+        },
+      });
     }),
 
   create: protectedProcedure
     .input(insertProjectSchema)
     .mutation(({ ctx, input }) => {
-      // return ctx.db.insert(schema.projects).values(input);
+      return ctx.db.insert(schema.projects).values(input);
     }),
 
   delete: protectedProcedure.input(z.string()).mutation(({ ctx, input }) => {
-    // return ctx.db.delete(schema.projects).where(eq(schema.projects.id, input));
+    return ctx.db.delete(schema.projects).where(eq(schema.projects.id, input));
   }),
+
+  addTask: protectedProcedure
+    .input(insertTaskSchema)
+    .mutation(({ ctx, input }) => {
+      console.log("iiiiii", input);
+      return ctx.db.insert(schema.tasks).values(input);
+    }),
 });
--- packages/api/src/projects/schema.ts
+++ packages/api/src/projects/schema.ts
@@ -1,16 +1,26 @@
 import { relations } from "drizzle-orm";
-import { json, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { json, pgEnum, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { nanoid } from "nanoid";
 
+export const todoStatusEnum = pgEnum("todo_status", [
+  "",
+  "not_started",
+  "in_progress",
+  "in_review",
+  "done",
+  "archived",
+]);
+
 export const projects = pgTable("project", {
-  id: text("id").default(nanoid()).notNull().primaryKey(),
+  id: text("id")
+    .$defaultFn(() => nanoid())
+    .default("")
+    .notNull()
+    .primaryKey(),
   name: text("name").notNull(),
+  slug: text("slug").notNull().unique().default(""),
   createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
-
-  // userId: text("userId")
-  //   .notNull()
-  //   .references(() => users.id, { onDelete: "cascade" }),
 });
 
 export const projectsRelations = relations(projects, ({ many }) => ({
@@ -18,26 +28,123 @@ export const projectsRelations = relations(projects, ({ many }) => ({
 }));
 
 export const tasks = pgTable("task", {
-  id: text("id").default(nanoid()).notNull().primaryKey(),
+  id: text("id")
+    .$defaultFn(() => nanoid())
+    .default("")
+    .notNull()
+    .primaryKey(),
   name: text("name").notNull(),
+  status: todoStatusEnum("status").default("").notNull(),
+  dueDate: timestamp("due_date").array(2),
   description: text("description"),
   content: json("content"),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
   completedAt: timestamp("completed_at"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
   updatedAt: timestamp("updated_at").defaultNow().notNull(),
 
-  // userId: text("userId")
-  //   .notNull()
-  //   .references(() => users.id, { onDelete: "cascade" }),
-
-  projectId: text("project_id").references(() => projects.id, {
-    onDelete: "cascade",
-  }),
+  parentId: text("parent_id"),
+  projectId: text("project_id")
+    .references(() => projects.id, {
+      onDelete: "cascade",
+    })
+    .default("")
+    .notNull(),
 });
 
-export const tasksRelations = relations(tasks, ({ one }) => ({
+export const tasksRelations = relations(tasks, ({ one, many }) => ({
   project: one(projects, {
     fields: [tasks.projectId],
     references: [projects.id],
   }),
+  parrent: one(tasks, {
+    fields: [tasks.parentId],
+    references: [tasks.id],
+  }),
+  children: many(tasks),
 }));
+
+// [
+//     {
+//         "id": 40896,
+//         "updated_at": "2023-12-29T01:10:03.507325+08:00",
+//         "name": "Test",
+//         "description": null,
+//         "estimated_workload": 0.0,
+//         "estimated_start_time": "2023-12-04T01:00:00+08:00",
+//         "estimated_end_time": "2023-12-07T00:59:59.999000+08:00",
+//         "actual_workload": 0.0,
+//         "actual_start_time": "2023-12-28T08:00:00+08:00",
+//         "actual_end_time": "2023-12-29T00:53:43.351934+08:00",
+//         "status": 99,
+//         "risk": 40,
+//         "order": 2,
+//         "is_deleted": false,
+//         "type": "story",
+//         "number_of_subtask": 1,
+//         "number_of_unfinished_subtask": 0,
+//         "subtask_estimated_workload": 0.0,
+//         "subtask_actual_workload": 0.0,
+//         "parent_path": "",
+//         "parent_id": null,
+//         "calculate_workload_from_subtask": true,
+//         "thirdparty": "",
+//         "thirdparty_id": "",
+//         "created_at": "2023-12-04T12:09:48.936366+08:00",
+//         "team": 262,
+//         "sprint": null,
+//         "assignee": null,
+//         "creator": 7933,
+//         "enterprise": 497,
+//         "collaborators": [],
+//         "team_id": 262,
+//         "sprint_id": null,
+//         "assignee_id": null,
+//         "collaborator_ids": [],
+//         "tags": [],
+//         "successor_ids": [],
+//         "project_permission": 3,
+//         "children": [
+//             {
+//                 "id": 40897,
+//                 "updated_at": "2023-12-04T12:15:12.854215+08:00",
+//                 "name": "test sub",
+//                 "description": "jkhkjh\nasd\nas\nd",
+//                 "estimated_workload": 0.0,
+//                 "estimated_start_time": "2023-12-04T01:00:00+08:00",
+//                 "estimated_end_time": "2023-12-07T00:59:59.999000+08:00",
+//                 "actual_workload": 0.0,
+//                 "actual_start_time": "2023-12-04T08:00:00+08:00",
+//                 "actual_end_time": "2023-12-04T12:10:47.581238+08:00",
+//                 "status": 99,
+//                 "risk": 0,
+//                 "order": 1,
+//                 "is_deleted": false,
+//                 "type": "bug",
+//                 "number_of_subtask": 0,
+//                 "number_of_unfinished_subtask": 0,
+//                 "subtask_estimated_workload": 0.0,
+//                 "subtask_actual_workload": 0.0,
+//                 "parent_path": "40896/",
+//                 "parent_id": 40896,
+//                 "calculate_workload_from_subtask": false,
+//                 "thirdparty": "",
+//                 "thirdparty_id": "",
+//                 "created_at": "2023-12-04T12:10:21.493075+08:00",
+//                 "team": 262,
+//                 "sprint": null,
+//                 "assignee": null,
+//                 "creator": 7933,
+//                 "enterprise": 497,
+//                 "collaborators": [],
+//                 "team_id": 262,
+//                 "sprint_id": null,
+//                 "assignee_id": null,
+//                 "collaborator_ids": [],
+//                 "tags": [],
+//                 "successor_ids": [],
+//                 "project_permission": 3,
+//                 "children": []
+//             }
+//         ]
+//     }
+// ]

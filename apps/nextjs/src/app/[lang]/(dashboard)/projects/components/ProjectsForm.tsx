a1c8ba932e8a931170fd1d7ad299da37f06e71e5
VyDucTan <vdt5snet@gmail.com>
2024-01-12 14:21:51 +0700

    feat(projects): create new component ProjectsForm to replace AddProjectForm and handle both create and update project functionality

---
+++ apps/nextjs/src/app/[lang]/(dashboard)/projects/components/ProjectsForm.tsx
@@ -0,0 +1,102 @@
+import { useEffect } from "react";
+
+import type { CreateProjectParams } from "@vyductan/api/types";
+import { insertProjectSchema } from "@vyductan/api/types";
+import { AutoForm, Loader, Modal, useForm } from "@vyductan/ui";
+
+import { api } from "~/trpc/react";
+
+type Props = {
+  id: string | undefined;
+  isOpen: boolean;
+  onCancel: () => void;
+  onOpenChange: (open: boolean) => void;
+};
+const ProjectModalForm = ({ id, isOpen, onCancel, onOpenChange }: Props) => {
+  const utils = api.useUtils();
+  const { data, isPending } = api.english.byId.useQuery(
+    {
+      id: id!,
+    },
+    {
+      enabled: !!id,
+    },
+  );
+  const createProject = api.projects.create.useMutation({
+    onSuccess: async () => {
+      // form.reset();
+      // setTitle("");
+      // setContent("");
+      await utils.projects.all.invalidate();
+    },
+    onError: (error) => {
+      console.log("error", error);
+      // form.setError(error)
+    },
+  });
+  const updateProject = api.projects.update.useMutation({
+    onSuccess: async () => {
+      // form.reset();
+      // setTitle("");
+      // setContent("");
+      await utils.projects.all.invalidate();
+    },
+    onError: (error) => {
+      console.log("error", error);
+      // form.setError(error)
+    },
+  });
+
+  const form = useForm<CreateProjectParams>({
+    schema: insertProjectSchema,
+    defaultValues: {},
+    onSubmit: async (values) => {
+      !id
+        ? createProject.mutate(values)
+        : updateProject.mutate({
+            ...values,
+            id,
+          });
+    },
+  });
+  const { setFieldsValue, resetFields } = form;
+  useEffect(() => {
+    if (data) {
+      setFieldsValue(data);
+    } else {
+      resetFields();
+    }
+  }, [data, setFieldsValue, resetFields]);
+
+  return (
+    <Modal
+      open={isOpen}
+      title={`${!id ? "Add" : "Edit"} Project`}
+      className="w-screen-md"
+      trigger={<button>XXX</button>}
+      onOk={form?.submit}
+      onCancel={onCancel}
+      onOpenChange={onOpenChange}
+    >
+      {!isOpen ? null : id && isPending ? (
+        <>
+          <Loader />
+        </>
+      ) : (
+        <AutoForm
+          form={form}
+          fields={[
+            {
+              type: "text",
+              name: "name",
+              label: "Name",
+              placeholder: "Enter the project name",
+            },
+          ]}
+        />
+      )}
+    </Modal>
+  );
+};
+
+export default ProjectModalForm;

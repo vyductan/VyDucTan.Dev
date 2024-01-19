4737bb0e53ea539bfbcf975a2b93fff0af3714fa
VyDucTan <vdt5snet@gmail.com>
2024-01-19 23:59:07 +0700

    chore(utils): remove unused files
    - api.ts
    
    - serverAction.ts
    
    - serverFn.ts
    
    - trpc-provider.tsx

--- apps/nextjs/src/utils/api.ts
+++
@@ -1,7 +0,0 @@
-import { createTRPCReact } from "@trpc/react-query";
-
-import type { AppRouter } from "@vyductan/api";
-
-export const api = createTRPCReact<AppRouter>();
-
-export { type RouterInputs, type RouterOutputs } from "@vyductan/api";

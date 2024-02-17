e22f0cd8d033a9430f68f18659687eabdca9b62e
Vy Duc Tan <vdt5snet@gmail.com>
2024-02-18 01:53:30 +0700

    fix: update middleware to handle authentication and redirection logic (#24)
    The middleware in the client code has been updated to handle authentication and redirection logic. 
    
    - The `authorized` callback has been added to check if a user is authorized to access protected routes. If a user is not logged in and tries to access a protected route, they will be redirected to the sign-in page with the callback URL appended. If a user is already logged in and tries to access the sign-in page, they will be redirected to the home page.
    
    - The `middleware` function has been updated to include the `auth` middleware from the `@vyductan/api/auth` package. This middleware handles authentication and sets the `x-url` header in the response. The `I18nMiddleware` function is then called with the updated request object.
    
    - The `matcher` in the server configuration has been updated to exclude the `image` directory from the routing.

--- apps/nextjs/src/middleware.ts
+++ apps/nextjs/src/middleware.ts
@@ -1,18 +1,23 @@
 import type { NextRequest } from "next/server";
 import { createI18nMiddleware } from "next-international/middleware";
 
+import { auth } from "@vyductan/api/auth";
+
 const I18nMiddleware = createI18nMiddleware({
   locales: ["en", "vi"],
   defaultLocale: "en",
 });
 
-export function middleware(request: NextRequest) {
-  // To get current url in server side
-  const response = I18nMiddleware(request);
-  response.headers.set("x-url", request.url);
+export async function middleware(request: NextRequest) {
+  const response = auth((req) => {
+    // To get current url in server side
+    const i18nResponse = I18nMiddleware(req);
+    i18nResponse.headers.set("x-url", req.url);
+    return i18nResponse;
+  })(request, {});
   return response;
 }
 
 export const config = {
-  matcher: ["/((?!api|static|.*\\..*|_next|favicon.ico|robots.txt).*)"],
+  matcher: ["/((?!api|static|_next|image|favicon.ico).*)"],
 };

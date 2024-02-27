3698dbc05cc9fd886cd715b4aa0bde647fd18ad3
VyDucTan <vdt5snet@gmail.com>
2024-02-27 16:11:49 +0700

    feat(Divider): Divide inline text
    The changes in this commit fix a styling issue with the Divider component. Previously, the component did not properly handle the case when children were provided. This caused the children to be rendered outside of the Divider, resulting in incorrect spacing and alignment.
    
    To fix this issue, the code has been modified to wrap the Divider and children in a container element. This ensures that the children are properly aligned and spaced within the Divider. Additionally, the styling of the Divider has been adjusted to correctly handle the orientation and size of the component.
    
    These changes improve the overall appearance and functionality of the Divider component.

--- ui/src/divider/Divider.tsx
+++ ui/src/divider/Divider.tsx
@@ -10,22 +10,38 @@ const Separator = React.forwardRef<
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(
   (
-    { className, orientation = "horizontal", decorative = true, ...props },
+    {
+      className,
+      orientation = "horizontal",
+      decorative = true,
+      children,
+      ...props
+    },
     ref,
-  ) => (
-    <SeparatorPrimitive.Root
-      ref={ref}
-      decorative={decorative}
-      orientation={orientation}
-      className={clsm(
-        "my-6",
-        "shrink-0 bg-border",
-        orientation === "horizontal" ? "h-[1px] w-full" : "h-full w-[1px]",
-        className,
-      )}
-      {...props}
-    />
-  ),
+  ) => {
+    const separator = (
+      <SeparatorPrimitive.Root
+        ref={ref}
+        decorative={decorative}
+        orientation={orientation}
+        className={clsm(
+          "my-6",
+          "shrink-0 bg-border",
+          orientation === "horizontal" ? "h-[1px] flex-1" : "h-full w-[1px]",
+          className,
+        )}
+        {...props}
+      />
+    );
+    const Comp = children ? "div" : React.Fragment;
+    return (
+      <Comp {...(children ? { className: "flex items-center gap-2" } : {})}>
+        {separator}
+        {children && <div className="mb-px">{children}</div>}
+        {children && separator}
+      </Comp>
+    );
+  },
 );
 Separator.displayName = SeparatorPrimitive.Root.displayName;
 

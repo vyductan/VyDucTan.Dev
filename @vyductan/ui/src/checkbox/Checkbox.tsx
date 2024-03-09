f9fd27f0fe359eeb2dd347a3fe6f4d90f3bef1e7
VyDucTan <vdt5snet@gmail.com>
2024-03-09 14:26:50 +0700

    refactor(checkbox): update Checkbox component to support indeterminate state and improve accessibility and styling

--- @vyductan/ui/src/checkbox/Checkbox.tsx
+++ @vyductan/ui/src/checkbox/Checkbox.tsx
@@ -6,33 +6,55 @@ import { Icon } from "../icons";
 
 type CheckboxProps = Omit<
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
-  "onChange" | "onCheckedChange"
+  "checked" | "defaultChecked" | "onChange" | "onCheckedChange"
 > & {
+  checked?: boolean;
+  defaultChecked?: boolean;
+  indeterminate?: boolean;
   onChange?: (checked: boolean) => void;
 };
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   CheckboxProps
->(({ className, onChange, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={clsm(
-      "peer size-4 shrink-0 rounded-sm border border-primary ring-offset-background",
-      "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
-      "disabled:cursor-not-allowed disabled:opacity-50",
-      "data-[state=checked]:bg-primary data-[state=checked]:text-white",
-      className,
-    )}
-    onCheckedChange={onChange}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={clsm("flex items-center justify-center text-current")}
-    >
-      <Icon icon="mingcute:check-fill" className="size-[14px]" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-));
+>(
+  (
+    { checked, defaultChecked, className, indeterminate, onChange, ...props },
+    ref,
+  ) => {
+    const id = React.useId();
+    return (
+      <label className={clsm("inline-flex", className)} htmlFor={id}>
+        <CheckboxPrimitive.Root
+          id={id}
+          ref={ref}
+          checked={indeterminate ? "indeterminate" : checked}
+          defaultChecked={indeterminate ? "indeterminate" : defaultChecked}
+          className={clsm(
+            "peer size-4 shrink-0 self-center rounded-[4px]",
+            "border border-ds-gray-500 bg-background ring-offset-background",
+            "disabled:cursor-not-allowed disabled:opacity-50",
+            "data-[state=checked]:border-primary data-[state=checked]:bg-primary data-[state=checked]:text-white",
+            "data-[state=indeterminate]:text-ds-gray-900",
+            "hover:border-ds-gray-700",
+            "focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2",
+          )}
+          onCheckedChange={onChange}
+          {...props}
+        >
+          <CheckboxPrimitive.Indicator
+            className={clsm("flex items-center justify-center text-current")}
+          >
+            {indeterminate ? (
+              <Icon icon="ant-design:x-filled" className="size-[10px]" />
+            ) : (
+              <Icon icon="mingcute:check-fill" className="size-[14px]" />
+            )}
+          </CheckboxPrimitive.Indicator>
+        </CheckboxPrimitive.Root>
+      </label>
+    );
+  },
+);
 Checkbox.displayName = CheckboxPrimitive.Root.displayName;
 
 export { Checkbox };

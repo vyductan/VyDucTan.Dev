cc97255c05c008aa27d0390b79f688c2f67372cf
vyductan <vdt5snet@gmail.com>
2024-08-28 09:59:20 +0700

    refactor(nextjs): Remove unused code in Tanstack Route
    Removed unused code related to route definition and type declarations in
    order to clean up the codebase and improve maintainability.

--- apps/nextjs/src/routes/config.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const searchSchema = z.object({
-  query: z.string().catch(""),
-  page: z.number().catch(1),
-  pageSize: z
-    .union([z.literal(10), z.literal(20), z.literal(50), z.literal(100)])
-    .catch(10),
-});
